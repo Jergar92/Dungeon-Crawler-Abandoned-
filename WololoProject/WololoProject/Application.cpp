96ce95035dbcb836b750019e88aadf6129c4ac9e
Jordior97 <jordior97@gmail.com>
2016-06-29 18:55:29 +0200

    Application , Globals.cpp and main added

---
+++ WololoProject/WololoProject/Application.cpp
@@ -0,0 +1,59 @@
+#include "Application.h"
+#include "ModuleWindow.h"
+
+Application::Application()
+{
+	int i = 0;
+	modules[i++] = window = new ModuleWindow();
+}
+
+Application::~Application()
+{
+	for (int i = NUM_MODULES - 1; i >= 0; --i)
+		delete modules[i];
+}
+
+bool Application::Init()
+{
+	bool ret = true;
+
+	// Deactivate modules here ----
+
+
+
+	// ----------------------------
+
+	for (int i = 0; i < NUM_MODULES && ret == true; ++i)
+		ret = modules[i]->Init();
+
+	for (int i = 0; i < NUM_MODULES && ret == true; ++i)
+		ret = modules[i]->IsEnabled() ? modules[i]->Start() : true;
+
+	return ret;
+}
+
+update_status Application::Update()
+{
+	update_status ret = UPDATE_CONTINUE;
+
+	for (int i = 0; i < NUM_MODULES && ret == UPDATE_CONTINUE; ++i)
+		ret = modules[i]->IsEnabled() ? modules[i]->PreUpdate() : UPDATE_CONTINUE;
+
+	for (int i = 0; i < NUM_MODULES && ret == UPDATE_CONTINUE; ++i)
+		ret = modules[i]->IsEnabled() ? modules[i]->Update() : UPDATE_CONTINUE;
+
+	for (int i = 0; i < NUM_MODULES && ret == UPDATE_CONTINUE; ++i)
+		ret = modules[i]->IsEnabled() ? modules[i]->PostUpdate() : UPDATE_CONTINUE;
+
+	return ret;
+}
+
+bool Application::CleanUp()
+{
+	bool ret = true;
+
+	for (int i = NUM_MODULES - 1; i >= 0 && ret == true; --i)
+		ret = modules[i]->IsEnabled() ? modules[i]->CleanUp() : true;
+
+	return ret;
+}
