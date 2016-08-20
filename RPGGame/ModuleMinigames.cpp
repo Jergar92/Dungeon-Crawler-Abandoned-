02350c2592bdc59f80eb5366949543f1b35b8097
elliotjb <elliot.jimenez1@gmail.com>
2016-08-20 21:06:00 +0200

    Add puzzle level 3
    In this puzzle u have organizate the imatge.

--- RPGGame/ModuleMinigames.cpp
+++ RPGGame/ModuleMinigames.cpp
@@ -20,6 +20,16 @@ ModuleMinigames::ModuleMinigames()
 	Botton_4 = { 403, 206, 29, 29 };
 	Botton_5 = { 517, 206, 29, 29 };
 
+	puzzle_1 = { 0, 0, 133, 133 };
+	puzzle_2 = { 133, 0, 133, 133 };
+	puzzle_3 = { 266, 0, 133, 133 };
+	puzzle_4 = { 0, 133, 133, 133 };
+	puzzle_5 = { 133, 133, 133, 133 };
+	puzzle_6 = { 266, 133, 133, 133 };
+	puzzle_7 = { 0, 266, 133, 134 };
+	puzzle_8 = { 133, 266, 133, 134 };
+	puzzle_9 = { 266, 266, 133, 134 };
+
 }
 
 ModuleMinigames::~ModuleMinigames()
@@ -30,17 +40,27 @@ bool ModuleMinigames::Start()
 	graphics = App->texture->Load("Puzzle_lvl1_pi.png");
 	graphics_info = App->texture->Load("Puzzle_lvl1_pi_info.png");
 	g_numbers = App->texture->Load("Numbers.png");
+	Puzzle = App->texture->Load("Sprite_lvl3.png");
 
 	for (int i = 0; i < 5; i++)
 	{
 		num[i] = 0;
 	}
+
+	for (int i = 0, j = 8; i < 9; i++, j--)
+	{
+		num_puzzle[j] = i;
+	}
+
 	puzzles_complets[0] = false;
 	puzzles_complets[1] = false;
 	puzzles_complets[2] = false;
 	puzzles_complets[3] = false;
 
-
+	for (int i = 0; i < 9; i++)
+	{
+		isenable[i] = false;
+	}
 
 	return true;
 }
@@ -60,13 +80,13 @@ update_status ModuleMinigames::Update()
 		{
 			numbers_game();
 		}
-		else if (puzzles_complets[1] == false)
+		/*else if (puzzles_complets[1] == false)
 		{
-
-		}
+			puzzles_complets[1] = true;
+		}*/
 		else if (puzzles_complets[2] == false)
 		{
-
+			puzzle_game();
 		}
 		else if (puzzles_complets[3] == false)
 		{
@@ -204,8 +224,191 @@ void ModuleMinigames::simon_game()
 
 }
 
-void ModuleMinigames::puzzle_game(){
+void ModuleMinigames::puzzle_game()
+{
+	if (App->player->dir == NORTH)
+	{
 
+	}
+	else if (App->player->dir == EAST)
+	{
+
+		for (int j = 0; j < 9; j++)
+		{
+			for (int i = 0; i < 9; i++)
+			{
+				int number_puzzle = 10;
+				if (num_puzzle[j] == i)
+				{
+					number_puzzle = i;
+				}
+
+				if (number_puzzle != 10)
+				{
+					int x = 0, y = 0;
+					if (j == 1)
+					{
+						x = 133;
+					}
+					if (j == 2)
+					{
+						x = 266;
+					}
+					if (j == 3)
+					{
+						y = 133;
+					}
+					if (j == 4)
+					{
+						x = 133;
+						y = 133;
+					}
+					if (j == 5)
+					{
+						x = 266;
+						y = 133;
+					}
+					if (j == 6)
+					{
+						y = 266;
+					}
+					if (j == 7)
+					{
+						x = 133;
+						y = 266;
+					}
+					if (j == 8)
+					{
+						x = 266;
+						y = 266;
+					}
+
+					if (number_puzzle == 0)
+					{
+						App->render->Blit(Puzzle, x + 100, y + 90, &puzzle_1);
+					}
+					if (number_puzzle == 1)
+					{
+						App->render->Blit(Puzzle, x + 100, y + 90, &puzzle_2);
+					}
+					if (number_puzzle == 2)
+					{
+						App->render->Blit(Puzzle, x + 100, y + 90, &puzzle_3);
+					}
+					if (number_puzzle == 3)
+					{
+						App->render->Blit(Puzzle, x + 100, y + 90, &puzzle_4);
+					}
+					if (number_puzzle == 4)
+					{
+						App->render->Blit(Puzzle, x + 100, y + 90, &puzzle_5);
+					}
+					if (number_puzzle == 5)
+					{
+						App->render->Blit(Puzzle, x + 100, y + 90, &puzzle_6);
+					}
+					if (number_puzzle == 6)
+					{
+						App->render->Blit(Puzzle, x + 100, y + 90, &puzzle_7);
+					}
+					if (number_puzzle == 7)
+					{
+						App->render->Blit(Puzzle, x + 100, y + 90, &puzzle_8);
+					}
+					if (number_puzzle == 8)
+					{
+						App->render->Blit(Puzzle, x + 100, y + 90, &puzzle_9);
+					}
+				}
+			}
+		}
+		
+		if (App->input->mouse_buttons[SDL_BUTTON_LEFT] == KEY_STATE::KEY_DOWN) // if the user clicked a mousebutton
+		{
+
+			if (App->gui->CheckButton(&puzzle_1, App->input->mouse_x - 100, App->input->mouse_y - 90))
+			{
+				isenable[0] = true;
+			}
+			if (App->gui->CheckButton(&puzzle_2, App->input->mouse_x - 100, App->input->mouse_y - 90))
+			{
+				isenable[1] = true;
+			}
+			if (App->gui->CheckButton(&puzzle_3, App->input->mouse_x - 100, App->input->mouse_y - 90))
+			{
+				isenable[2] = true;
+			}
+			if (App->gui->CheckButton(&puzzle_4, App->input->mouse_x - 100, App->input->mouse_y - 90))
+			{
+				isenable[3] = true;
+			}
+			if (App->gui->CheckButton(&puzzle_5, App->input->mouse_x - 100, App->input->mouse_y - 90))
+			{
+				isenable[4] = true;
+			}
+			if (App->gui->CheckButton(&puzzle_6, App->input->mouse_x - 100, App->input->mouse_y - 90))
+			{
+				isenable[5] = true;
+			}
+			if (App->gui->CheckButton(&puzzle_7, App->input->mouse_x - 100, App->input->mouse_y - 90))
+			{
+				isenable[6] = true;
+			}
+			if (App->gui->CheckButton(&puzzle_8, App->input->mouse_x - 100, App->input->mouse_y - 90))
+			{
+				isenable[7] = true;
+			}
+			if (App->gui->CheckButton(&puzzle_9, App->input->mouse_x - 100, App->input->mouse_y - 90))
+			{
+				isenable[8] = true;
+			}
+		}
+
+		int j = 0;
+
+		for (int i = 0; i < 9; i++)
+		{
+			if (isenable[i] == true)
+			{
+				j++;
+				if (j > 1)
+				{
+					swap_1 = i;
+				}
+				else
+				{
+					swap_2 = i;
+				}
+			}
+		}
+
+		if (j > 1)
+		{
+			int temp = num_puzzle[swap_1];
+			num_puzzle[swap_1] = num_puzzle[swap_2];
+			num_puzzle[swap_2] = temp;
+			for (int i = 0; i < 9; i++)
+			{
+				isenable[i] = false;
+			}
+		}
+
+		
+	}
+	else if (App->player->dir == SOUTH)
+	{
+
+	}
+	else if (App->player->dir == WEST)
+	{
+
+	}
+
+	if (num_puzzle[0] == 0 && num_puzzle[1] == 1 && num_puzzle[2] == 2 && num_puzzle[3] == 3 && num_puzzle[4] == 4
+		&& num_puzzle[5] == 5 && num_puzzle[6] == 6 && num_puzzle[7] == 7 && num_puzzle[8] == 8)
+	{
+		puzzles_complets[2] = true;
+	}
 
 
 
--- RPGGame/ModuleMinigames.h
+++ RPGGame/ModuleMinigames.h
@@ -21,7 +21,7 @@ public:
 	SDL_Texture* graphics = nullptr;
 	SDL_Texture* g_numbers = nullptr;
 	SDL_Texture* graphics_info = nullptr;
-
+	SDL_Texture* Puzzle = nullptr;
 
 	//numbers
 
@@ -34,20 +34,37 @@ public:
 	SDL_Rect Botton_3;
 	SDL_Rect Botton_4;
 	SDL_Rect Botton_5;
-
 	bool puzzles_complets[4];
 	int num[5];
-	void three_line_game();
 	void numbers_game();
-	void simon_game();
+
+
+	//Level 3 Puzzle
+	SDL_Rect puzzle_1;
+	SDL_Rect puzzle_2;
+	SDL_Rect puzzle_3;
+	SDL_Rect puzzle_4;
+	SDL_Rect puzzle_5;
+	SDL_Rect puzzle_6;
+	SDL_Rect puzzle_7;
+	SDL_Rect puzzle_8;
+	SDL_Rect puzzle_9;
 	void puzzle_game();
+	int num_puzzle[9];
+	bool isenable[9];
+	int swap_1 = 0;
+	int swap_2 = 0;
+
+
+	void three_line_game();
+	void simon_game();
+
 	/*char mat[3][3];
 	void Iniciar_tablero(char mat[3][3]);
 	void Tablero(const char mat[3][3]);
 	void Yo_tirada(char mat[3][3]);
 	void Pc_tirada(char mat[3][3]);
 	void funciones(char mat[3][3]);*/
-
 
 	//Textures
 };
